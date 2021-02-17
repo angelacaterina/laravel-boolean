3b21bc67feadce1586753670a09e98052e78e4af
angelacaterina <angela.caterina@icloud.com>
2021-02-17 17:34:43 +0100

    database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller{
     //
     public function home(){
         return view('home');
@@ -13,7 +13,9 @@ class HomeController extends Controller
     public function about(){
         return view('about');
     }
-    public function blog(){
+    public function blog(Post $post){
+        $posts = $post->all();
+        dd($posts);
         return view('blog');
     }
 }
